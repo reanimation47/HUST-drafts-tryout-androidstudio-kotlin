00544e19195700d50a2409da9e5621164f60ea6f
DannnyLe <danny.le@playstudios.asia>
2024-10-31 19:52:10 +0700

    wip gmail

---
+++ app/src/main/java/com/example/hust_example_mobile_app_kotlin/SimpleChatApp.kt
@@ -0,0 +1,20 @@
+package com.example.hust_example_mobile_app_kotlin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SimpleChatApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_simple_chat_app)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
